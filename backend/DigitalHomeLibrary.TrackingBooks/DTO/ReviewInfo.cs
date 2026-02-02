334dab494cce23bf0cfea093cb64c31bd8dfaabc
Ivan <thefuny@mail.ru>
2026-02-02 04:51:10 +0300

    Update ReviewInfo.cs

--- backend/DigitalHomeLibrary.TrackingBooks/DTO/ReviewInfo.cs
+++ backend/DigitalHomeLibrary.TrackingBooks/DTO/ReviewInfo.cs
@@ -4,6 +4,6 @@ namespace DigitalHomeLibrary.TrackingBooks.DTO
 {
     public record ReviewInfo(byte Score, string Note, BookInfo ReviewedBook)
     {
-        public static ReviewInfo FromEntity(Review review) => new(review.Score, review.Note, BookInfo.FromEntity(review.ReviewedBook));
+        public static ReviewInfo FromEntity(Review review) => new(review.Score, review.Note, review.ReviewedBook is not null ? BookInfo.FromEntity(review.ReviewedBook) : throw new InvalidCastException($"Review with id = {review.Id} has no book"));
     }
 }
